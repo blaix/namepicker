216cc5c8ec82cee3a8028b58717938d74bf69a3c
Justin Blake <jblake@navidson.local>
2009-02-07 11:43:32 -0500

    total complete mindblowing refactoring

--- namepicker.rb
+++ namepicker.rb
@@ -3,10 +3,11 @@ require 'sinatra'
 
 get '/' do
   names = File.read('./names.txt').strip.split(/\r|\n/)
+  name = names[rand(names.length)].upcase
   haml <<-EOS
 %html
   %body
     %strong OMG THE WINNER IS...
-    %h1 #{names[rand(names.length)].upcase} OMG!
+    %h1 #{name} OMG!
   EOS
 end
